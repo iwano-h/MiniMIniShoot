5f4a01a202535dc35e387604bf1f35536ba95a79
岩野宏行 <>
2021-01-29 23:48:01 +0900

    タイトルシーンにSE、スタートボタンにChangeメソッドを実装 ゲームシーンの床プレファブに着手中

---
+++ Assets/Scripts/Title/StartButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void Change()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Click()
+    {
+        GetComponent<AudioSource>().Play();
+        Invoke("Change", 1f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
